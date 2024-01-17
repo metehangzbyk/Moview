3692549b979f8ddedc74fd619f9320b6b5bc78d1
Mete <metehan_gzbyk@hotmail.com>
2024-01-17 13:18:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+const API_URL =
+  'https:api.themoviedb.org/4/discover/movie?sort_by=popularity.desc&api_key=d6e3451ed5a6294a458ec67b8256e537&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API =
+//   'https://api.themoviedb.org/4/search/movie?api_key=d6e3451ed5a6294a458ec67b8256e537&query='
+  'https://api.themoviedb.org/3/search/movie?api_key=d6e3451ed5a6294a458ec67b8256e537&query='
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const main = document.getElementById('main')
+
+getMovies(API_URL)
+
+async function getMovies(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  console.log(data.results)
+  showMovies(data.results)
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const searchTerm = search.value
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm)
+
+    search.value = ''
+  } else {
+    window.location.reload()
+  }
+})
+
+function showMovies(movies) {
+  main.innerHTML = ''
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie
+
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+
+    movieEl.innerHTML = `
+    
+    <img
+    src="${IMG_PATH + poster_path}" alt="${title}"
+  />
+  <div class="movie-info">
+    <h3>${title}</h3>
+    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+  </div>
+  <div class="overview">
+    <h3> ${title}  <small> Overview </small> </h3>
+    <p>
+     ${overview}
+    </p>
+  </div>
+
+    `
+
+    main.appendChild(movieEl)
+  })
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green'
+  } else if (vote >= 5) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
+}
